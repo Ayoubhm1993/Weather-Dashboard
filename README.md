5e6cf260d28aa79f10105aecf2e8d015419776ea
Ayoubhm1993 <Ayoubhm1993@gmail.com>
2020-11-02 03:58:27 -0600

    update README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Weather-Dashboard
+## Purpose
+
+A website that offers Daily and hourly weather by city and geolocaltion.
+
+## ScreenShot
+
+![555555](https://user-images.githubusercontent.com/70945176/97854708-73ad4a80-1cbf-11eb-8d0c-18d5562454dc.jpg)
+
+
+## Built With
+*HTML
+*CSS
+*JAVASCRIPT
+*BOOTSTRAP
+*JQUERY
+*API
+
+## Website Link 
+
+https://ayoubhm1993.github.io/Weather-Dashboard
+
+## Repository Link
+
+https://github.com/Ayoubhm1993/Weather-Dashboard
+
+## Contribution
+
+Made with ❤️ by Ayoub
--- assest/js/script.js
+++ assest/js/script.js
@@ -7,14 +7,14 @@ let windSpeed=document.querySelector('#wind-speed');
 let description=document.querySelector('#uvIndex');
 let searchForm=document.querySelector('#user-form');
 let citySearch=document.querySelector("#search");
-
+let icons=document.querySelector('.icon');
 let placeDisplay=document.querySelector('.city');
 
 let city=citySearch.value.trim();
 /*let api='http://api.openweathermap.org/data/2.5/weather?q='+{city}+'&APPID=';*/
 function myFunction (city){
 
- let   api='http://api.openweathermap.org/data/2.5/weather?q=';
+ let   api='https://api.openweathermap.org/data/2.5/weather?q=';
  let   key=`${city}&appid=0964eb5ff2a7dff34c27cd743bb02d0a`;
  
  
@@ -39,7 +39,7 @@ function myFunction (city){
 function displayWeather(data){
     
 
-link="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=0964eb5ff2a7dff34c27cd743bb02d0a";
+link="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=0964eb5ff2a7dff34c27cd743bb02d0a";
 
 var requestJson =new XMLHttpRequest();
 
@@ -50,11 +50,12 @@ requestJson.onload=function(){
     var obj=JSON.parse(this.response);
     if (requestJson.status>=200 && requestJson.status<400) {
 
+icons.innerHTML=`<div class="icon">${obj.weather[0].id}</div>`
 temp.innerHTML=`<div id="temperature"  ><h3>Temperature : ${Math.floor(((obj.main.temp)-273.15)*(9/5)+32)} F / ${Math.floor((obj.main.temp)-273.15)}  C</h3></div>`;
 humidity.innerHTML=`<div id="humidity"><h3>Humidity :  ${obj.main.humidity}</h3></div>`;
 windSpeed.innerHTML=`<div id="wind-speed" ><h3>Wind Speed :  ${obj.wind.speed}</h3></div>`;
 loca.innerHTML=`<div id="location" class="city" >
-                         ${obj.name}
+                         <h3>${obj.name}</h3>
                           <div id="description">
                            <h3>Description:  ${obj.weather[0].description}</h3>
                           </div>
@@ -69,6 +70,7 @@ loca.innerHTML=`<div id="location" class="city" >
 requestJson.send()
     
 };
+
 
 
 button.addEventListener("click",myFunction(city));
